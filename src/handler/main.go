b37912ecc53c29d90d27e7ef736ff37dc3affc90
MeiWagatsuma <wagamei256@gmail.com>
2023-03-05 18:58:18 +0900

    Update error log

--- src/handler/main.go
+++ src/handler/main.go
@@ -3,10 +3,6 @@ package handler
 
 import (
 	"echo-todo-server/src/model"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -21,8 +17,11 @@ func ResponseHelloWorld(c echo.Context) error {
 func GetTodoList(c echo.Context) error {
 	todos, err := model.SelectTodoList()
 	if err != nil {
-		fmt.Printf("Failed to reading the todo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
+	}
+
+	if len(todos) == 0 {
+		return c.String(http.StatusNotFound, "Todo was not found!")
 	}
 
 	return c.JSON(http.StatusOK, todos)
@@ -30,41 +29,28 @@ func GetTodoList(c echo.Context) error {
 
 // CreateTodo is for making a new Todo
 func CreateTodo(c echo.Context) error {
-	fmt.Println("create todo")
-	todo := model.Todo{}
-
-	body, err := io.ReadAll(c.Request().Body)
-	if err != nil {
-		log.Printf("Failed to reading the request body for addTodo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+	todo := new(model.Todo)
+	if err := c.Bind(todo); err != nil {
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
 	}
 
-	if err = json.Unmarshal(body, &todo); err != nil {
-		log.Printf("Failed unmarshaling n CreateTodo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+	if err := todo.CreateTodo(); err != nil {
+		return c.String(http.StatusInternalServerError, "Invalid value.")
 	}
 
-	if err = todo.CreateTodo(); err != nil {
-		log.Printf("Failed creating todo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
-	}
-
-	fmt.Printf("this is your todo: %#v", todo)
-	return c.String(http.StatusOK, "Create todo was succeeded!")
+	return c.String(http.StatusOK, "Successfully added todo!")
 }
 
 // UpdateTodo is for update todo
 func UpdateTodo(c echo.Context) error {
 	todo := new(model.Todo)
 	if err := c.Bind(todo); err != nil {
-		log.Printf("Failed binding to todo for update: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
 	}
 
 	id := c.Param("id")
 	if err := todo.UpdateTodo(id); err != nil {
-		log.Printf("Failed updateing todo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+		return c.String(http.StatusBadRequest, "Invalid id.")
 	}
 
 	return c.String(http.StatusOK, "Update todo was succeeded!")
@@ -74,15 +60,13 @@ func UpdateTodo(c echo.Context) error {
 func DeleteTodo(c echo.Context) error {
 	todo := new(model.Todo)
 	if err := c.Bind(todo); err != nil {
-		log.Printf("Failed binding to todo for delete: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
 	}
 
 	id := c.Param("id")
 	if err := todo.DeleteTodo(id); err != nil {
-		log.Printf("Failed deleting todo: %s", err)
-		return c.String(http.StatusInternalServerError, "")
+		return c.String(http.StatusBadRequest, "Invalid id.")
 	}
 
-	return c.String(http.StatusOK, "Delete todo was succeeded!")
+	return c.String(http.StatusOK, "Successfully deleted todo!")
 }
