de4daafbc5d15d64c89db1ea52cca68b4f0df989
MeiWagatsuma <wagamei256@gmail.com>
2023-03-06 12:45:23 +0900

    Move handler to group

--- src/group/todo.go
+++ src/group/todo.go
@@ -2,15 +2,74 @@
 package group
 
 import (
-	"echo-todo-server/src/handler"
+	"echo-todo-server/src/model"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 // Todo has all todo methods
 func Todo(g *echo.Group) {
-	g.GET("", handler.GetTodoList)
-	g.POST("", handler.CreateTodo)
-	g.PUT(":id", handler.UpdateTodo)
-	g.DELETE(":id", handler.DeleteTodo)
+	g.GET("", getTodoList)
+	g.POST("", createTodo)
+	g.PUT(":id", updateTodo)
+	g.DELETE(":id", deleteTodo)
+}
+
+// GetTodoList returns a list todo for a repository
+func getTodoList(c echo.Context) error {
+	todos, err := model.Read()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
+	}
+
+	if len(todos) == 0 {
+		return c.String(http.StatusNotFound, "Todo was not found!")
+	}
+
+	return c.JSON(http.StatusOK, todos)
+}
+
+// CreateTodo is for making a new Todo
+func createTodo(c echo.Context) error {
+	todo := new(model.Todo)
+	if err := c.Bind(todo); err != nil {
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
+	}
+
+	if err := todo.Create(); err != nil {
+		return c.String(http.StatusInternalServerError, "Invalid value.")
+	}
+
+	return c.String(http.StatusOK, "Successfully added todo!")
+}
+
+// UpdateTodo is for update todo
+func updateTodo(c echo.Context) error {
+	todo := new(model.Todo)
+	if err := c.Bind(todo); err != nil {
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
+	}
+
+	id := c.Param("id")
+	if err := todo.Update(id); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid id.")
+	}
+
+	return c.String(http.StatusOK, "Update todo was succeeded!")
+}
+
+// DeleteTodo is for delete todo
+func deleteTodo(c echo.Context) error {
+	todo := new(model.Todo)
+	if err := c.Bind(todo); err != nil {
+		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
+	}
+
+	id := c.Param("id")
+	if err := todo.Delete(id); err != nil {
+		return c.String(http.StatusBadRequest, "Invalid id.")
+	}
+
+	return c.String(http.StatusOK, "Successfully deleted todo!")
 }
--- src/handler/main.go
+++ src/handler/main.go
@@ -2,7 +2,6 @@
 package handler
 
 import (
-	"echo-todo-server/src/model"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,62 +10,4 @@ import (
 // ResponseHelloWorld is for testing for responses of api
 func ResponseHelloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, world!")
-}
-
-// GetTodoList returns a list todo for a repository
-func GetTodoList(c echo.Context) error {
-	todos, err := model.Read()
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
-	}
-
-	if len(todos) == 0 {
-		return c.String(http.StatusNotFound, "Todo was not found!")
-	}
-
-	return c.JSON(http.StatusOK, todos)
-}
-
-// CreateTodo is for making a new Todo
-func CreateTodo(c echo.Context) error {
-	todo := new(model.Todo)
-	if err := c.Bind(todo); err != nil {
-		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
-	}
-
-	if err := todo.Create(); err != nil {
-		return c.String(http.StatusInternalServerError, "Invalid value.")
-	}
-
-	return c.String(http.StatusOK, "Successfully added todo!")
-}
-
-// UpdateTodo is for update todo
-func UpdateTodo(c echo.Context) error {
-	todo := new(model.Todo)
-	if err := c.Bind(todo); err != nil {
-		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
-	}
-
-	id := c.Param("id")
-	if err := todo.Update(id); err != nil {
-		return c.String(http.StatusBadRequest, "Invalid id.")
-	}
-
-	return c.String(http.StatusOK, "Update todo was succeeded!")
-}
-
-// DeleteTodo is for delete todo
-func DeleteTodo(c echo.Context) error {
-	todo := new(model.Todo)
-	if err := c.Bind(todo); err != nil {
-		return c.String(http.StatusInternalServerError, "Oops! Something went wrong!")
-	}
-
-	id := c.Param("id")
-	if err := todo.Delete(id); err != nil {
-		return c.String(http.StatusBadRequest, "Invalid id.")
-	}
-
-	return c.String(http.StatusOK, "Successfully deleted todo!")
 }
