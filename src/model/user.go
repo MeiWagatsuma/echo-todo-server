a6c8c34cd171d34574baddce34e5a61aaa409133
MeiWagatsuma <wagamei256@gmail.com>
2023-03-06 19:06:04 +0900

    Fix table name

--- src/model/user.go
+++ src/model/user.go
@@ -3,7 +3,7 @@ package model
 import "log"
 
 func CreateUserTable() (err error) {
-	query := `CREATE TABLE IF NOT EXISTS todos (
+	query := `CREATE TABLE IF NOT EXISTS users (
 		id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 		name VARCHAR(255) NOT NULL,
 		password VARCHAR(255) NOT NULL,
